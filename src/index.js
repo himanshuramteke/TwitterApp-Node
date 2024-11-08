79fbd351bb54158074abf3d769b6a4d4aa4e4d58
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-11-08 19:46:01 +0530

    Added ejs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import morgan from 'morgan';
 
 //Create a new express app/server object
 const app = express();
+console.log(import.meta);
+app.set('view engine', 'ejs');
+
+app.set('views', import.meta.dirname + '/views');
 
 app.use(morgan('combined'));
 
