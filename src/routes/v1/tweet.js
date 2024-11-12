1bf045947d0b76bc3912d1282bcc0cc4bbb4d6a9
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-11-12 22:27:55 +0530

    Added controller layer

--- src/routes/v1/tweet.js
+++ src/routes/v1/tweet.js
@@ -1,19 +1,12 @@
 import express from 'express';
+import { getTweetById, getTweets, createTweet } from "../../controllers/tweetControllers.js";
 
 const router = express.Router(); //create a new router object;
 
-router.get('/', (req, res) => {
-    return res.json({
-        message: 'Welcome to the tweet route'
-    });
-});
+router.get('/', getTweets)
 
-router.get('/:id', (req, res) => {
-    return res.json({
-        message: 'Welcome to the tweet route',
-        id: req.params.id
-    });
-})
+router.get('/:id', getTweetById);
+
+router.post('/', createTweet);
 
 export default router; //Export default router object
-
